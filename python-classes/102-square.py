42b63b88b7327a94cf103dec09afe1f1f4653e4b
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-03-03 03:57:09 -0800

    square

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -1,21 +1,23 @@
+
+
 #!/usr/bin/python3
 """Magic class from a given ByteCode."""
 import math
 
 
 class MagicClass:
-    """Initialization of the MagicClass."""
+ """Initialization of the MagicClass."""
     def __init__(self, radius=0):
-        """Magic class constructor."""
+        """magic class constructor"""
         self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self._MagicClass__radius = radius
 
     def area(self):
-        """Calculate the area."""
+        """Calculation of the area."""
         return self._MagicClass__radius ** 2 * math.pi
 
     def circumference(self):
-        """Calculate the circumference."""
+        """Calculation of the circumference."""
         return 2 * math.pi * self._MagicClass__radius
