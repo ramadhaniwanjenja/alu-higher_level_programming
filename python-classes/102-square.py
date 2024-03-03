a6b4582cbeedc5ef78de2d2b674657ee3445ef16
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-03-03 04:26:31 -0800

    square

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -1,23 +1,52 @@
-
-
 #!/usr/bin/python3
-"""Magic class from a given ByteCode."""
-import math
+"""Square class defination"""
 
 
-class MagicClass:
- """Initialization of the MagicClass."""
-    def __init__(self, radius=0):
-        """magic class constructor"""
-        self._MagicClass__radius = 0
-        if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
-        self._MagicClass__radius = radius
+class Square:
+    """square body"""
+
+    def __init__(self, size=0):
+        """square contructor 
+        Args: size: length of a side of Square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """"The propery of size as the length 
+        of a side of Square
+        Raises:
+            TypeError: if size != int
+            ValueErrorr: if size < 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     def area(self):
-        """Calculation of the area."""
-        return self._MagicClass__radius ** 2 * math.pi
+        """Get the area instance to comparators"""
+        return self.__size * self.__size
 
-    def circumference(self):
-        """Calculation of the circumference."""
-        return 2 * math.pi * self._MagicClass__radius
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
