c97f5f41d466682b0c6d070b679a948a2013a837
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-03-03 03:51:08 -0800

    square

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -1,52 +1,21 @@
 #!/usr/bin/python3
-"""Square class defination"""
+"""Magic class from a given ByteCode."""
+import math
 
 
-class Square:
-    """square body"""
-
-    def __init__(self, size=0):
-        """square contructor 
-        Args: size: length of a side of Square
-        """
-        self.__size = size
-
-    @property
-    def size(self):
-        """"The propery of size as the length 
-        of a side of Square
-        Raises:
-            TypeError: if size != int
-            ValueErrorr: if size < 0
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+class MagicClass:
+    """Initialization of the MagicClass."""
+    def __init__(self, radius=0):
+        """Magic class constructor."""
+        self._MagicClass__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
 
     def area(self):
-        """Get the area instance to comparators"""
-        return self.__size * self.__size
+        """Calculate the area."""
+        return self._MagicClass__radius ** 2 * math.pi
 
-    def __le__(self, other):
-        return self.area() <= other.area()
-
-    def __lt__(self, other):
-        return self.area() < other.area()
-
-    def __ge__(self, other):
-        return self.area() >= other.area()
-
-    def __ne__(self, other):
-        return self.area() != other.area()
-
-    def __gt__(self, other):
-        return self.area() > other.area()
-
-    def __eq__(self, other):
-        return self.area() == other.area()
+    def circumference(self):
+        """Calculate the circumference."""
+        return 2 * math.pi * self._MagicClass__radius
