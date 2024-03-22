6e867eef5b8dd1172c0e430880ff9cb797a93e56
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-03-22 11:49:49 -0700

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status."""
+"""
+Fetches https://alu-intranet.hbtn.io/status
+"""
+
 import urllib.request
 
+def fetch_status():
+    """
+    Fetches the status from https://alu-intranet.hbtn.io/status
+    """
+    url = "https://alu-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode("utf-8"))
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    fetch_status()
+
