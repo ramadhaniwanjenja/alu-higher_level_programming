d8fd7fb6ad3ab7c95ae2bb5474b460184c3567bd
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-03-22 12:28:36 -0700

    status

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://alu-intranet.hbtn.io/status using requests"""
+"""fetches http://0.0.0.0:5050/status using requests"""
 import requests
 
 
 if __name__ == "__main__":
-    """fetches https://alu-intranet.hbtn.io/status using requests"""
-    r = requests.get('https://alu-intranet.hbtn.io/status')
+    """fetches http://0.0.0.0:5050/status using requests"""
+    r = requests.get('http://0.0.0.0:5050/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
+
