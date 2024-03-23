6d48b82fa7ce7307731ea6ac0556ab358f253141
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-03-22 18:31:10 -0700

    github commit

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""fetches http://0.0.0.0:5050/status using requests"""
+"""Fetches https://alu-intranet.hbtn.io/status using requests"""
 import requests
 
-
 if __name__ == "__main__":
-    """fetches http://0.0.0.0:5050/status using requests"""
-    r = requests.get('http://0.0.0.0:5050/status')
+    """Fetches https://alu-intranet.hbtn.io/status using requests"""
+    r = requests.get('https://alu-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
-
