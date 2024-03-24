36f75f9769c27c6833b76c0f89af4605d12d2e6f
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-03-24 01:27:56 -0700

    git commits

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -3,20 +3,19 @@
 Python script that shows the last 10 commits of a repository
 in GitHub
 """
+from requests import get, auth
 import sys
-import requests
+
 
 if __name__ == "__main__":
     try:
         repo = sys.argv[1]
         owner = sys.argv[2]
         url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-        r = requests.get(url)
-        r.raise_for_status()  # Raise an error if request fails
+        r = get(url)
         json_o = r.json()
-        for commit in json_o:
-            sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
-            print("{}: {}".format(sha, author_name))
-    except Exception as e:
-        print("Error:", e)
+        for i in range(0, 10):
+            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
+                                  .get('author').get('name')))
+    except:
+        pass
