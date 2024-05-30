96e84fa1ca23b98fc1dc15196888a202d93a06cc
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-05-30 17:06:26 +0200

    add me maybe

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 function callMeMoby (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-      theFunction();
-    }
+  for (let i = 0; i < x; i++) {
+    theFunction();
   }
-  
-  module.exports = { callMeMoby };
+}
+
+module.exports = { callMeMoby };
