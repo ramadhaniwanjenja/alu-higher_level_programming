425ef15e289ddbe10e434c391fa0a1f23d98f353
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-05-31 03:37:56 -0700

    interger

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-
-const arg = process.argv[2]; // Get the first argument
-const number = parseInt(arg, 10); // Try to convert it to an integer
-
-if (!isNaN(number)) {
-  console.log(`My number: ${number}`);
-} else {
-  console.log('Not a number');
-}
-
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
