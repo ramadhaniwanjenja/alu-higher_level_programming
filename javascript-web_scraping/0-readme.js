0c6d34eba3d74b5d1b7416185d74b762dc4b4851
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-06-05 04:15:09 -0700

    readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-
-const request = require('fs');
-
-const filePath = process.argv[2];
-
-request.readFile(filePath, function (err, data) {
-  if (err) console.log(err);
-  const content = data;
-  console.log(content.toString());
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
